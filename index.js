be63cb80c34720cb2884658ce37fce68c192aea7
Asim-gohar <asimbutt9664@gmail.com>
2023-08-04 02:27:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+"use strict";
+/*
+// which console will be operate first
+var userName : string= "asim";
+var num1 : number = 10 ;
+var num2 : number =20 ;
+var num3 : number =25 ;
+
+// all console are now
+console.log(num3);
+console.log(num1);
+console.log(userName);
+console.log(num2);
+*/
+/*
+var userName : string= "asim";
+var userName2 : string= 'Gohar';
+// this is the Template literate that have more functions
+var userName3 : string= `Butt${userName}`;
+console.log(userName);
+console.log(userName3);
+console.log(userName2);
+*/
+// its for to find type of var
+/*
+var userName : string="Asim";
+var userNum : number= 31 ;
+console.log(typeof userNum);
+*/
+//Arthimetical operators are: + , _ ,* , / , **(for exponent)
+/*
+var num1 :number= 10;
+var num2 :number= 2;
+var result : number = num1 + num2 ;
+var result1 : number = num1**num2
+console.log(result , result1);
+*/
+/*
+// Assingnment Arthimetical operators are: += , -= ,*= , /= , % .
+var num1 : number= 2 ;
+var num2 : number= 3 ;
+num1 /= 6
+console.log(num1)
+// reminder step is missing
+
+var result : number = num1 % num2
+console.log(result);
+*/
+// previous expression used to understand comparison operators
+var num1 = 2;
+var num2 = 7;
+var num3 = 8;
+var result = num1 + num2 - num3 * 2 / 4;
+console.log(result);
+/*
+// There are 5 Real comparison operators that is == , !=, <, >, ===.
+var num1:number= 5;
+var num2:number= 4;
+var output:boolean = num1 == num2
+console.log(output);
+*/
+/*
+// logicals operators example like phy
+// And & operator
+   T && T => T
+   F && F => F
+   F && T => F
+   T && F => F
+
+// AUR | operators
+T | T => T
+F | T => T
+T | F => T
+F | F => F
+*/
+/*
+// Logic operator in practice
+var num1:number= 5;
+var num2:number= 10;
+var output:boolean = 5==5 && num1==num2
+console.log(output);
+*/
